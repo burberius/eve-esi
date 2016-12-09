34556f12cf7fe6227dbf11d2a583c6286d316671
Jens Oberender <burberius@gmail.com>
2016-12-09 22:55:19 +0100

    Changed to bleeding edge code generation.

--- generate.sh
+++ generate.sh
@@ -37,16 +37,16 @@ fi
 #
 # Get swagger code generator
 #
-VERSION=$(git ls-remote --tags https://github.com/swagger-api/swagger-codegen.git | grep -o "refs/tags/v.*" | sort -rV | head -1 | sed -e 's#.*v##')
+#VERSION=$(git ls-remote --tags https://github.com/swagger-api/swagger-codegen.git | grep -o "refs/tags/v.*" | sort -rV | head -1 | sed -e 's#.*v##')
 
-if [ ! -e swagger-codegen-cli-$VERSION.jar ]; then
-  wget -q http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/$VERSION/swagger-codegen-cli-$VERSION.jar
+#if [ ! -e swagger-codegen-cli-$VERSION.jar ]; then
+#  wget -q http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/$VERSION/swagger-codegen-cli-$VERSION.jar
 
-  if [ $? != 0 ]; then
-    echo "Could not download http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/$VERSION/swagger-codegen-cli-$VERSION.jar"
-    exit -1
-  fi
-fi
+#  if [ $? != 0 ]; then
+#    echo "Could not download http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/$VERSION/swagger-codegen-cli-$VERSION.jar"
+#    exit -1
+#  fi
+#fi
 
 #
 # Transform and beautify swagger file
@@ -57,7 +57,7 @@ sed -i -f replace.sed esi.json
 #
 # Generate code
 #
-java -jar swagger-codegen-cli-$VERSION.jar generate \
+java -jar swagger-codegen-cli.jar generate \
   -i esi.json \
   -l java \
   -c config.json
