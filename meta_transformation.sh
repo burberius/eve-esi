b1aba13ea1cc8584a7726ac7b6563af13a579a17
GoldenGnu <GoldenGnu@users.noreply.github.com>
2024-07-12 16:47:14 +0200

    ESI Update 2024-07-11 (#142)
    * Force codegen version 6.6.0
    
    Minor changes to meta transformation
    
    * OpenAPI document version: 1.24
    
    * Update 2024-07-11
    
    Ref: https://github.com/esi/esi-issues/blob/master/changelog.md#2024-07-11
    
    CharacterAssetsResponse.LocationFlagEnum > Added new entries
    CharacterNotificationsResponse.TypeEnum > Added new entries
    CharacterRolesResponse.RolesEnum & RolesAtBaseEnum & RolesAtHqEnum & RolesAtOtherEnum > Added new entries
    CharacterWalletJournalResponse.RefTypeEnum > Added new entries
    CorporationAssetsResponse.LocationFlagEnum > Added new entries
    CorporationRolesHistoryResponse.NewRolesEnum & OldRolesEnum > Added new entries
    CorporationRolesResponse.GrantableRolesEnum & GrantableRolesAtBaseEnum & GrantableRolesAtHqEnum & GrantableRolesAtOtherEnum & RolesEnum & RolesAtBaseEnum & RolesAtHqEnum & RolesAtOtherEnum > Added new entries
    CorporationTitlesResponse.GrantableRolesEnum & GrantableRolesAtBaseEnum & GrantableRolesAtHqEnum & GrantableRolesAtOtherEnum & RolesEnum & RolesAtBaseEnum & RolesAtHqEnum & RolesAtOtherEnum > Added new entries
    CorporationWalletJournalResponse.RefTypeEnum > Added new entries
    
    * Fixed tests
    
    * Added GitHub Action secrets
    
    * Schedule GitHub Action to run every Monday @ 8:44

--- meta_transformation.sh
+++ meta_transformation.sh
@@ -3,10 +3,12 @@
 # evesso securityDefinitions
 echo "Added evesso securityDefinitions"
 jq "(.securityDefinitions.evesso) = {\"authorizationUrl\": \"https://login.eveonline.com/v2/oauth/authorize\",\"flow\": \"implicit\",\"scopes\": {}, \"type\": \"oauth2\"}" meta.json > meta_1.json
+mv meta_1.json meta.json
 
 # evesso verify
 echo "Added evesso to verify"
-jq "(.paths.\"/verify/\".get.security[0].evesso) = {}" meta_1.json > meta.json
+jq "(.paths.\"/verify/\".get.security[0].evesso) = {}" meta.json > meta_1.json
+mv meta_1.json meta.json
 
 # Clean up
 rm meta_1.json
