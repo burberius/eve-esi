23bd34b5e12e343201e65b579143000c6a7a0f5d
Jens Oberender <burberius@users.noreply.github.com>
2025-08-11 15:33:57 +0200

    First steps

--- generate.sh
+++ generate.sh
@@ -10,11 +10,11 @@ rm -f esi.json
 #
 # esi _latest
 #
-wget -O esi.json https://esi.evetech.net/_latest/swagger.json?datasource=tranquility || exit 1
+wget -O esi.json https://esi.evetech.net/meta/openapi.json || exit 1
 #
 # meta
 #
-wget -O meta.json https://esi.evetech.net/swagger.json || exit 1
+#wget -O meta.json https://esi.evetech.net/swagger.json || exit 1
 
 #
 # Get swagger code generator
@@ -26,39 +26,39 @@ if [ ! -e openapi-generator-cli-$VERSION.jar ]; then
 
   if [ $? != 0 ]; then
     echo "Could not download https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/$VERSION/openapi-generator-cli-$VERSION.jar"
-    exit -1
+    exit 1
   fi
 fi
 
 #
 # Remove old model files in case something was removed
 #
-mv src/main/java/net/troja/eve/esi/model/CharacterInfo.java src/main/java/net/troja/eve/esi/CharacterInfo.java
+#mv src/main/java/net/troja/eve/esi/model/CharacterInfo.java src/main/java/net/troja/eve/esi/CharacterInfo.java
 rm -r src/main/java/net/troja/eve/esi/model
-mv src/main/java/net/troja/eve/esi/api/SsoApi.java src/main/java/net/troja/eve/esi/SsoApi.java
+#mv src/main/java/net/troja/eve/esi/api/SsoApi.java src/main/java/net/troja/eve/esi/SsoApi.java
 rm -r src/main/java/net/troja/eve/esi/api
 
 #
 # Transform and beautify swagger file
 #
 sed -i -f replace.sed esi.json
-sed -i -f meta_replace.sed meta.json
+#sed -i -f meta_replace.sed meta.json
 
-./meta_transformation.sh
+#./meta_transformation.sh
 ### -!- Workaround continue in transformation.sh
-./transformation.sh
+#./transformation.sh
 #
 # Generate code
 # Move tests so they are generated new and then moved to a different directory
 #
 test -d src/test/java/net/troja/eve/esi/api.new && rm -r src/test/java/net/troja/eve/esi/api.new
 mv src/test/java/net/troja/eve/esi/api src/test/java/net/troja/eve/esi/api.old
-java -jar openapi-generator-cli-$VERSION.jar generate \
-  --skip-validate-spec  \
-  -i meta.json \
-  -t templates \
-  -g java \
-  -c config.json
+#java -jar openapi-generator-cli-$VERSION.jar generate \
+#  --skip-validate-spec  \
+#  -i meta.json \
+#  -t templates \
+#  -g java \
+#  -c config.json
 java -jar openapi-generator-cli-$VERSION.jar generate \
   --skip-validate-spec  \
   -i esi.json \
@@ -67,49 +67,49 @@ java -jar openapi-generator-cli-$VERSION.jar generate \
   -c config.json
 mv src/test/java/net/troja/eve/esi/api src/test/java/net/troja/eve/esi/api.new
 mv src/test/java/net/troja/eve/esi/api.old src/test/java/net/troja/eve/esi/api
-mv src/main/java/net/troja/eve/esi/CharacterInfo.java src/main/java/net/troja/eve/esi/model/CharacterInfo.java
-mv src/main/java/net/troja/eve/esi/SsoApi.java src/main/java/net/troja/eve/esi/api/SsoApi.java
+#mv src/main/java/net/troja/eve/esi/CharacterInfo.java src/main/java/net/troja/eve/esi/model/CharacterInfo.java
+#mv src/main/java/net/troja/eve/esi/SsoApi.java src/main/java/net/troja/eve/esi/api/SsoApi.java
 
 # Fix route
-jq ".paths | keys" esi.json > version-routes.txt
+#jq ".paths | keys" esi.json > version-routes.txt
 
 #
 # Generate SSO scopes
 #
-BAD_SCOPES=""
-FILE="src/main/java/net/troja/eve/esi/auth/SsoScopes.java"
-echo "package net.troja.eve.esi.auth;" > $FILE
-echo "" >> $FILE
-echo "import java.util.Arrays;" >> $FILE
-echo "import java.util.Collections;" >> $FILE
-echo "import java.util.HashSet;" >> $FILE
-echo "import java.util.Set;" >> $FILE
-echo "" >> $FILE
-echo "public class SsoScopes {" >> $FILE
-echo "public static final String PUBLIC_DATA = \"publicData\";" >> $FILE
-for VAL in $(jq "(.paths[][]? | select(.security[0].evesso).security[0].evesso[0])" esi.json | sort | uniq | sed -e 's#"##g'); do
-  echo $BAD_SCOPES | grep $VAL > /dev/null && continue
-  UPPER=$(echo $VAL | tr [.a-z-] [_A-Z_])
-  if [ "a$ALL" = "a" ]; then
-    ALL="$UPPER"
-  else
-    ALL="$ALL, $UPPER"
-  fi
-  echo "public static final String $UPPER = \"$VAL\";" >> $FILE
-done
-echo -e "\nprivate static final String[] ALL_VALUES = {$ALL};" >> $FILE
-echo "" >> $FILE
-echo "    public static Set<String> ALL = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(ALL_VALUES)));" >> $FILE
-echo "" >> $FILE
-echo "}" >> $FILE
+#BAD_SCOPES=""
+#FILE="src/main/java/net/troja/eve/esi/auth/SsoScopes.java"
+#echo "package net.troja.eve.esi.auth;" > $FILE
+#echo "" >> $FILE
+#echo "import java.util.Arrays;" >> $FILE
+#echo "import java.util.Collections;" >> $FILE
+#echo "import java.util.HashSet;" >> $FILE
+#echo "import java.util.Set;" >> $FILE
+#echo "" >> $FILE
+#echo "public class SsoScopes {" >> $FILE
+#echo "public static final String PUBLIC_DATA = \"publicData\";" >> $FILE
+#for VAL in $(jq "(.paths[][]? | select(.security[0].evesso).security[0].evesso[0])" esi.json | sort | uniq | sed -e 's#"##g'); do
+#  echo $BAD_SCOPES | grep $VAL > /dev/null && continue
+#  UPPER=$(echo $VAL | tr [.a-z-] [_A-Z_])
+#  if [ "a$ALL" = "a" ]; then
+#    ALL="$UPPER"
+#  else
+#    ALL="$ALL, $UPPER"
+#  fi
+#  echo "public static final String $UPPER = \"$VAL\";" >> $FILE
+#done
+#echo -e "\nprivate static final String[] ALL_VALUES = {$ALL};" >> $FILE
+#echo "" >> $FILE
+#echo "    public static Set<String> ALL = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(ALL_VALUES)));" >> $FILE
+#echo "" >> $FILE
+#echo "}" >> $FILE
 
 #
 # Clean formating
 #
 mvn formatter:format
 
-cp src/test/java/net/troja/eve/esi/api/GeneralApiTest.java src/test/java/net/troja/eve/esi/api.new/
-cp -r src/test/java/net/troja/eve/esi/api/auth src/test/java/net/troja/eve/esi/api.new/
+#cp src/test/java/net/troja/eve/esi/api/GeneralApiTest.java src/test/java/net/troja/eve/esi/api.new/
+#cp -r src/test/java/net/troja/eve/esi/api/auth src/test/java/net/troja/eve/esi/api.new/
 
 if (( $SECONDS >= 60 ))
    then
